57feb4eb6896d52ec2f3f2dd5e09e28ea0125326
niranjan549912 <niranjanacad549@gmail.com>
2022-09-04 23:55:42 -0500

    adding featureAddress2 field

--- TestOne/src/Feature2.java
+++ TestOne/src/Feature2.java
@@ -2,12 +2,11 @@ public class Feature2 {
 
     String sname;
     int sid;
+    String featureAddress2;
 
-    public Feature2(String sname, int sid) {
+    public Feature2(String sname, int sid, String featureAddress2) {
         this.sname = sname;
         this.sid = sid;
+        this.featureAddress2 = featureAddress2;
     }
-
-
-
 }
