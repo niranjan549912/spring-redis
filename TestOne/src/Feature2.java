581874298703854b387e6bb4c3db71380be8be5b
niranjan549912 <niranjanacad549@gmail.com>
2022-09-04 23:55:42 -0500

    # This is a combination of 5 commits. # This is the 1st commit message:
    adding featureAddress2 field
    
    # This is the commit message #2:
    
    adding feature3 file
    
    # This is the commit message #3:
    
    adding featureAddress3 field
    
    # This is the commit message #4:
    
    Feature 5 change added
    
    # This is the commit message #5:
    
    Feature 5 added sout new

--- TestOne/src/Feature2.java
+++ TestOne/src/Feature2.java
@@ -2,12 +2,13 @@ public class Feature2 {
 
     String sname;
     int sid;
+    String featureAddress2;
+    String featureAddress3;
 
-    public Feature2(String sname, int sid) {
+    public Feature2(String sname, int sid, String featureAddress2, String featureAddress3) {
         this.sname = sname;
         this.sid = sid;
+        this.featureAddress2 = featureAddress2;
+        this.featureAddress3 = featureAddress3;
     }
-
-
-
 }
---
+++ TestOne/src/Feature5.java
@@ -0,0 +1,22 @@
+public class Feature5 {
+
+    int fid;
+    String fname;
+    public Feature5() {
+    }
+
+    public Feature5(int fid) {
+        this.fid = fid;
+    }
+
+    public Feature5(String fname) {
+        this.fname = fname;
+    }
+
+    public Feature5(int fid, String fname) {
+        this.fid = fid;
+        this.fname = fname;
+
+        System.out.println("");
+    }
+}
