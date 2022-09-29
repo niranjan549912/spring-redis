adcdcf8de087b5b27206006af3c4c1d13b3e9342
niranjan549912 <niranjanacad549@gmail.com>
2022-09-29 07:09:04 -0500

    combined all commits

---
+++ TestOne/src/Feature4.java
@@ -0,0 +1,18 @@
+public class Feature4 {
+
+
+    int fid;
+    String fname;
+
+    public Feature4(int fid, String fname) {
+        this.fid = fid;
+        this.fname = fname;
+    }
+
+    public Feature4() {
+    }
+
+    public Feature4(int fid) {
+        this.fid = fid;
+    }
+}
